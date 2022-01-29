1b5b21f452a8771df8260aefbbf11486c38765ff
Sven-the-man <95774546+Sven-the-man@users.noreply.github.com>
2022-01-29 21:28:28 +0100

    Pagination

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,23 +1,47 @@
 <template>
   <div class="container">
+    <div class="navigation">
+      <b-pagination
+        :total-rows="totalRows"
+        v-model="currentPage"
+        :per-page="perPage"
+      />
+    </div>
     <main class="grid">
-      <article v-for="meal in meals" :key="meal.id">
+      <div class="article" v-for="meal in meals" :key="meal.id">
         <div class="text">
-          <router-link :to="{name: 'meal.show', params: {id: meal.id}}">
-          <img src="https://www.foodandfriends.nl/app/uploads/2020/12/Humburger-Kookschool-klassiek.jpg">
+          <router-link :to="{ name: 'meal.show', params: { id: meal.id } }">
+            <img
+              src="https://www.foodandfriends.nl/app/uploads/2020/12/Humburger-Kookschool-klassiek.jpg"
+            />
           </router-link>
-          <h3>{{meal.name}}</h3>
+          <h3>{{ meal.name }}</h3>
         </div>
-      </article>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      currentPage: 1,
+      perPage: 9,
+    };
+  },
+
   computed: {
     meals() {
-      return this.$store.getters["meals/getAll"];
+      const items = this.$store.getters["meals/getAll"];
+
+      return items.slice(
+        (this.currentPage - 1) * this.perPage,
+        this.currentPage * this.perPage
+      );
+    },
+    totalRows() {
+      return this.$store.getters["meals/getAll"].length;
     },
   },
   mounted() {
@@ -27,30 +51,32 @@ export default {
 </script>
 
 <style>
-
+.navigation {
+  padding: 20px;
+  margin: auto;
+}
 .container {
-    width: 60%;
-    
+  width: 60%;
 }
 .grid {
-  padding-top: 20px;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   grid-gap: 20px;
   align-items: stretch;
 }
-.grid > article {
+.grid > .article {
   border: 1px solid #ccc;
   box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.3);
   background: white;
 }
-.grid > article img {
+.grid > .article img {
   max-width: 100%;
   box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.3);
+  margin-bottom: 20px;
 }
 .text {
+  margin-top: 20px;
   padding: 20px;
   text-align: center;
 }
-
 </style>
