19ba07018fc01ae9ee363cb95b5e28a280d29e6f
Sven-the-man <95774546+Sven-the-man@users.noreply.github.com>
2022-01-29 21:10:30 +0100

    Bootstrap vue install

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,12 @@ import axios from 'axios';
 window.axios = axios;
 axios.defaults.baseURL = '/api/';
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 //Main pages
 import App from "./App.vue";
 
