21d9063d6d0b234cf9c6cae95aad37a35094919f
Sven-the-man <95774546+Sven-the-man@users.noreply.github.com>
2022-02-06 18:58:17 +0100

    Ingredients grouping

--- app/Http/resources/IngredientResource.php
+++ app/Http/resources/IngredientResource.php
@@ -16,7 +16,8 @@ class IngredientResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name
+            'name' => $this->name,
+            'category' => $this->category,
         ];
     }
 }
--- database/seeders/IngredientSeeder.php
+++ database/seeders/IngredientSeeder.php
@@ -14,15 +14,33 @@ class IngredientSeeder extends Seeder
      */
     public function run()
     {
-        Ingredient::create(['name' => 'Aardappelen']);
-        Ingredient::create(['name' => 'Uien']);
-        Ingredient::create(['name' => 'Sla']);
-        Ingredient::create(['name' => 'Rijst']);
-        Ingredient::create(['name' => 'Meel']);
-        Ingredient::create(['name' => 'Knoflook']);
-        Ingredient::create(['name' => 'Rundvlees']);
-        Ingredient::create(['name' => 'Varkensvlees']);
-        Ingredient::create(['name' => 'Paprika']);
-        Ingredient::create(['name' => 'Tomaten']);
+        Ingredient::create(['name' => 'Aardappelen', 'category' => 'Groenten']);
+        Ingredient::create(['name' => 'Uien', 'category' => 'Groenten']);
+        Ingredient::create(['name' => 'Sla', 'category' => 'Groenten']);
+        Ingredient::create(['name' => 'Rijst', 'category' => 'Groenten']);
+        Ingredient::create(['name' => 'Paprika', 'category' => 'Groenten']);
+        Ingredient::create(['name' => 'Tomaten', 'category' => 'Groenten']);
+       
+        Ingredient::create(['name' => 'Knoflook', 'category' => 'Kruiden']);
+        Ingredient::create(['name' => 'Gember', 'category' => 'Kruiden']);
+        Ingredient::create(['name' => 'Suiker', 'category' => 'Kruiden']);
+
+        Ingredient::create(['name' => 'Rundvlees', 'category' => 'Vlees']);
+        Ingredient::create(['name' => 'Varkensvlees', 'category' => 'Vlees']);
+        Ingredient::create(['name' => 'Kippenvlees', 'category' => 'Vlees']);
+        Ingredient::create(['name' => 'Lamsvlees', 'category' => 'Vlees']);
+
+        Ingredient::create(['name' => 'Ananas', 'category' => 'Fruit']);
+        Ingredient::create(['name' => 'Sinaasappels', 'category' => 'Fruit']);
+        Ingredient::create(['name' => 'Appels', 'category' => 'Fruit']);
+
+        Ingredient::create(['name' => 'Ketchup', 'category' => 'Saus']);
+        Ingredient::create(['name' => 'Mayonaise', 'category' => 'Saus']);
+        Ingredient::create(['name' => 'Curry', 'category' => 'Saus']);
+
+
+
+
+        
     }
 }
